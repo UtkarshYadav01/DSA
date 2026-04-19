8c15273e27cfb204948ec69c225196fd64068fb6
UtkarshYadav01 <67846943+UtkarshYadav01@users.noreply.github.com>
2026-04-20 01:20:04 +0530

    Java Stream Interview Questions 💡 Level Hard | Part 1

--- src/test/java/StreamAPI/StreamIntermediateLevelPracticeApril2026Test.java
+++ src/test/java/StreamAPI/StreamIntermediateLevelPracticeApril2026Test.java
@@ -3,26 +3,36 @@ package StreamAPI;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 class StreamIntermediateLevelPracticeApril2026Test {
 
-    StreamIntermediateLevelPracticeApril2026 test = new StreamIntermediateLevelPracticeApril2026();
+    StreamPracticeApril2026 test = new StreamPracticeApril2026();
 
-    List<StreamIntermediateLevelPracticeApril2026.Employee> employees = List.of(
-            new StreamIntermediateLevelPracticeApril2026.Employee("Candace", "HR", 60000),
-            new StreamIntermediateLevelPracticeApril2026.Employee("Bob", "IT", 90000),
-            new StreamIntermediateLevelPracticeApril2026.Employee("David", "IT", 110000),
-            new StreamIntermediateLevelPracticeApril2026.Employee("Sam", "IT", 9000),
-            new StreamIntermediateLevelPracticeApril2026.Employee("Alice", "HR", 70000),
-            new StreamIntermediateLevelPracticeApril2026.Employee("Eric", "Sales", 50000));
+    List<StreamPracticeApril2026.Employee> employees = List.of(
+            new StreamPracticeApril2026.Employee("Candace", "HR", 60000),
+            new StreamPracticeApril2026.Employee("Bob", "IT", 90000),
+            new StreamPracticeApril2026.Employee("David", "IT", 110000),
+            new StreamPracticeApril2026.Employee("Sam", "IT", 9000),
+            new StreamPracticeApril2026.Employee("Alice", "HR", 70000),
+            new StreamPracticeApril2026.Employee("Eric", "Sales", 50000),
+            new StreamPracticeApril2026.Employee("Adam","Salse",70000));
 
-    List<StreamIntermediateLevelPracticeApril2026.Person> persons = List.of(
-            new StreamIntermediateLevelPracticeApril2026.Person("Alice", 25),
-            new StreamIntermediateLevelPracticeApril2026.Person("Bob", 30),
-            new StreamIntermediateLevelPracticeApril2026.Person("Charlie", 28),
-            new StreamIntermediateLevelPracticeApril2026.Person("David", 35));
+    List<StreamPracticeApril2026.Person> persons = List.of(
+            new StreamPracticeApril2026.Person("Alice", 25),
+            new StreamPracticeApril2026.Person("Bob", 30),
+            new StreamPracticeApril2026.Person("Charlie", 28),
+            new StreamPracticeApril2026.Person("David", 35));
+
+    List<StreamPracticeApril2026.Transaction> transactions = Arrays.asList(
+            new StreamPracticeApril2026.Transaction("T1", LocalDate.of(2024, 1, 1), 300),
+            new StreamPracticeApril2026.Transaction("T2", LocalDate.of(2024, 1, 1), 200),
+            new StreamPracticeApril2026.Transaction("T3", LocalDate.of(2024, 1, 2), 500),
+            new StreamPracticeApril2026.Transaction("T4", LocalDate.of(2024, 1, 2), 300),
+            new StreamPracticeApril2026.Transaction("T5", LocalDate.of(2024, 1, 3), 100)
+    );
 
     @Test
     @DisplayName("Q1.Sort List of Employees By Salary")
@@ -31,9 +41,9 @@ class StreamIntermediateLevelPracticeApril2026Test {
     }
 
     @Test
-    @DisplayName("Q1_1.Sort List of Employees By Salary in reverse order")
-    void question1_1() {
-        System.out.println(test.question1_1(employees));
+    @DisplayName("Q1B.Sort List of Employees By Salary in reverse order")
+    void question1B() {
+        System.out.println(test.question1B(employees));
     }
 
 
@@ -73,32 +83,99 @@ class StreamIntermediateLevelPracticeApril2026Test {
 
     @Test
     @DisplayName("Q7.Find the highest-paid employee in each department")
-    void question11() {
+    void question7() {
         System.out.println(test.question7(employees));
     }
 
     @Test
     @DisplayName("Q8.Find All Departments with More than 2 Employees")
-    void question7() {
+    void question8() {
         System.out.println(test.question8(employees));
     }
 
     @Test
     @DisplayName("Q9.Find department with the highest average salary")
-    void question8() {
+    void question9() {
         System.out.println(test.question9(employees));
     }
 
     @Test
     @DisplayName("Q10.Find most frequent character in string")
-    void question9() {
+    void question10() {
         System.out.println(test.question10("employees"));
     }
 
     @Test
     @DisplayName("Q11.Find First non-repeating character in string")
-    void question10() {
+    void question11() {
         System.out.println(test.question11("employees"));
     }
 
+    //hard questions
+
+    @Test
+    @DisplayName("Q12.Find the most common first letter among all employee names")
+    void question12() {
+        System.out.println(test.question12(employees));
+    }
+
+    @Test
+    @DisplayName("Q13.Given a list of integers compute the average of every 3 element sliding window")
+    void question13() {
+        System.out.println(test.question13(Arrays.asList(4,8,15,16,23,42),3));
+    }
+
+    @Test
+    @DisplayName("Q14.Find the longest word in a sentence ignoring case and punctuation")
+    void question14() {
+        System.out.println(test.question14("The quick, brown fox jumped over the lazy dog!"));
+    }
+
+    @Test
+    @DisplayName("Q15.Find top 3 most frequent words in a paragraph")
+    void question15() {
+        System.out.println(test.question15("Java is great great. Java is object oriented. Java streams are powerful, and Java is fun!"));
+    }
+
+    @Test
+    @DisplayName("Q16.Reverse each word in a sentence using streams")
+    void question16() {
+        System.out.println(test.question16("The quick, brown fox jumped over the lazy dog!"));
+    }
+
+    @Test
+    @DisplayName("Q17.From a list of transactions, find the day with the highest total spend")
+    void question17() {
+        test.question17(transactions);
+    }
+
+    @Test
+    @DisplayName("Q18.Categorize employees based on their salary (Low/Medium/High) Using streams.")
+    void question18() {
+        test.question18();
+    }
+
+    @Test
+    @DisplayName("Q19.Group characters by uppercase vs lowercase vs digit vs other")
+    void question19() {
+        test.question19();
+    }
+
+    @Test
+    @DisplayName("Q20.Find all employees who worked in 3+ Departments")
+    void question20() {
+        test.question20();
+    }
+
+    @Test
+    @DisplayName("Q21.Find bigram frequency(Pairs of consecutive words) in Paragraphs")
+    void question21() {
+        test.question21();
+    }
+
+    @Test
+    @DisplayName("")
+    void question() {
+        test.question();
+    }
 }
